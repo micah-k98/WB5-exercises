8c9304f53f7e53e697291ce72ed3583fc729c20b
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-11-07 15:47:00 -0700

    Done with vehicle search exercise

---
+++ es6_vehicle_search.js
@@ -0,0 +1,84 @@
+"use strict"
+
+let vehicles = [
+    {
+    color: "Silver",
+    type: "Minivan",
+    registrationState: "CA",
+    licenseNo: "ABC-101",
+    registrationExpires: new Date("3-10-2022"),
+    capacity: 7
+    },
+    {
+    color: "Red",
+    type: "Pickup Truck",
+    registrationState: "TX",
+    licenseNo: "A1D-2NC",
+    registrationExpires: new Date("8-31-2023"),
+    capacity: 3
+    },
+    {
+    color: "White",
+    type: "Pickup Truck",
+    registrationState: "TX",
+    licenseNo: "A22-X00",
+    registrationExpires: new Date("9-31-2023"),
+    capacity: 6
+    },
+    {
+    color: "Red",
+    type: "Car",
+    registrationState: "CA",
+    licenseNo: "ABC-222",
+    registrationExpires: new Date("12-10-2022"),
+    capacity: 5
+    },
+    {
+    color: "Black",
+    type: "SUV",
+    registrationState: "CA",
+    licenseNo: "EEE-222",
+    registrationExpires: new Date("12-10-2021"),
+    capacity: 7
+    },
+    {
+    color: "Red",
+    type: "SUV",
+    registrationState: "TX",
+    licenseNo: "ZZ2-101",
+    registrationExpires: new Date("5-30-2022"),
+    capacity: 5
+    },
+    {
+    color: "White",
+    type: "Pickup Truck",
+    registrationState: "TX",
+    licenseNo: "CAC-7YT",
+    registrationExpires: new Date("1-31-2022"),
+    capacity: 5
+    },
+    {
+    color: "White",
+    type: "Pickup Truck",
+    registrationState: "CA",
+    licenseNo: "123-ABC",
+    registrationExpires: new Date("3-31-2023"),
+    capacity: 5
+    }
+];
+
+const isRed = vehicles.filter(v => v.color.toUpperCase() == "RED");
+console.log("Which vehicles are RED?");
+console.table(isRed);
+
+const expiredRegistration = vehicles.filter(v => v.registrationExpires <= new Date());
+console.log("Which vehicles have registrations that are expired?");
+console.table(expiredRegistration);
+
+const capacity = vehicles.filter(v => v.capacity >= 6);
+console.log("Which vehicles that hold at least 6 people?");
+console.table(capacity);
+
+const licensePlate = vehicles.filter(v => v.licenseNo.indexOf("222") != -1);
+console.log("Which vehicles have license plates that end with \"222\"?");
+console.table(licensePlate);
